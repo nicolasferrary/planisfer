c89490f71c790d07947dc88d23f01b57eee3379a
Coralie Collignon <coralie.collignon@gmail.com>
2017-02-27 18:50:47 +0100

    map on hover

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
- root to: 'pages#home'
- resources :searches, only: [:create, :show]
- resources :trips, only: [:index, :show, :update, :create]
- resources :round_trip_flights, only: [:create]
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :searches, only: [:create, :show]
+  resources :trips, only: [:index, :show, :update, :create]
+  resources :round_trip_flights, only: [:create] do
+    get :refresh_map, to: "searches#refresh_map"
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
