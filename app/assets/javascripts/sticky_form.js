e01df29e0e324a8ef198718b438bbba0a622acce
Coralie Collignon <coralie.collignon@gmail.com>
2017-03-14 16:38:12 +0100

    front with slider

--- app/assets/javascripts/sticky_form.js
+++ app/assets/javascripts/sticky_form.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
   $(".sticky-form").sticky({topSpacing:40});
-  $("#why").on("scroll", function(){
-    $(".sticky-form").unstick();
-  });
 });
+
+$.fn.followTo = function (pos) {
+    var $this = this,
+        $window = $(window);
+
+    $window.scroll(function (e) {
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        }    });
+};
+
+$('.sticky-form').followTo(1620);
