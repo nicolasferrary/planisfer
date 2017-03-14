d6d76260341aeae09b6f1e4bb3e84b94c4ea49ff
Nicolas Ferrary <ferraryn@gmail.com>
2017-03-14 13:44:07 +0100

    pending

--- app/services/Car rentals/smart_car_rental_agent.rb
+++ app/services/Car rentals/smart_car_rental_agent.rb
@@ -15,15 +15,18 @@ module Car_rental
           drop_off_place: @drop_off_place,
           pick_up_date_time: @pick_up_date_time,
           drop_off_date_time: @drop_off_date_time,
-          driver_age: @driver_age,
-
-          region_airport1: @region_airport1,
-          region_airport2: @region_airport2,
-          starts_on: @starts_on,
-          returns_on: @returns_on,
-          nb_travelers: @nb_travelers,
+          driver_age: @driver_age
           api_key: ENV['SKYSCANNER_API_KEY']
         ).make_request
+
+      @data = JSON.parse(json)
+
+      if !@data['trips']['tripOption'].nil?
+        rtf = create_rtf(@data['trips']['tripOption'])
+      else
+        rtf = []
+      end
+      @rtf = rtf
     end
 
     private
