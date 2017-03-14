32f4091362d049eb0074313afa9b43e2ce8c4bbe
Nicolas Ferrary <ferraryn@gmail.com>
2017-03-14 16:38:27 +0100

    pending

--- app/models/car_rental.rb
+++ app/models/car_rental.rb
@@ -5,41 +5,52 @@ class CarRental < ApplicationRecord
 
 
   class << self
-    def create(rental_data, pick_up_date_time, drop_off_date_time, driver_age, car)
+    def create(data, rental_data, pick_up_date_time, drop_off_date_time, driver_age, car)
       car_rental = CarRental.new
       car_rental.price = extract_price(rental_data)
-      car_rental.currency = extract_currency(rental_data)
+      car_rental.currency = extract_currency(data)
       car_rental.pick_up_address = extract_pick_up_address(rental_data)
       car_rental.drop_off_address = extract_drop_off_address(rental_data)
       car_rental.pick_up_date_time = pick_up_date_time
       car_rental.drop_off_date_time = drop_off_date_time
       car_rental.driver_age = driver_age
       car_rental.company = extract_company(rental_data)
-      car_rental.car = car_extract(rental_data) #différencier le cas ou la voiture existe et sinon créer une car en renvoyant à une méthode dans le modèle car
-      car_rental.car = deep_link_url_extract(rental_data)
+      car_rental.car = extract_car(data, rental_data)
+      car_rental.car = extract_deep_link_url(rental_data)
       car_rental.save
       car_rental
     end
 
-    def extract_currency(item)
-      item['saleTotal'].match(/\w{3}/).to_s
+
+    def extract_price(rental_data)
+      rental_data['price_all_days'].to_f
     end
 
-    def extract_price(item)
-      item['saleTotal'].match(/\d+\.*\d+/)[0].to_f
+    def extract_currency(data)
+      data['submitted_query']['currency']
     end
 
-    def extract_origin_airport_iata(item, slice)
-      item['slice'][slice]['segment'].first['leg'].first['origin']
+    def extract_pick_up_address(rental_data)
+      rental_data['location']['pick_up']['address']
     end
 
-    def extract_destination_airport_iata(item, slice)
-      item['slice'][slice]['segment'].first['leg'].first['destination']
+    def extract_drop_off_address(rental_data)
+      rental_data['location']['drop_off']['address']
     end
 
-    def extract_take_off_at(item, slice)
-      Time.parse item['slice'][slice]['segment'].first['leg'].first['departureTime']
+    def extract_company(rental_data)
+      rental_data['website_id']['name']
     end
+
+    def extract_car(data, rental_data)
+    #if car already exists in database
+      if !Car.find_by_name(rental_data['vehicle']).nil?
+        Car.find_by_name(rental_data['vehicle'])
+      else
+        Car.create(data, rental_data)
+      end
+    end
+
 
     def extract_landing_at(item, slice)
       Time.parse item['slice'][slice]['segment'].first['leg'].first['arrivalTime']
