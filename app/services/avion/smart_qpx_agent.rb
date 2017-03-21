b245ddf5adc23fcbd42d30a726309c4a2229893f
Nicolas Ferrary <ferraryn@gmail.com>
2017-03-21 17:10:31 +0100

    changed smart qpx agent to google flight

--- app/services/avion/smart_qpx_agent.rb
+++ app/services/avion/smart_qpx_agent.rb
@@ -1,5 +1,7 @@
 module Avion
-
+  # Query QPX two requests at a time, only make request if corresponding Offer
+  # is not found in Redis cache.
+  # TODO: remove debugging puts
   class SmartQPXAgent
     def initialize(args = {})
       @city = args[:city]
@@ -9,10 +11,21 @@ module Avion
       @starts_on = args[:starts_on]
       @returns_on = args[:returns_on]
       @nb_travelers = args[:nb_travelers]
+      # @cache_key_name = generate_cache_key_name
+      # while in development
+      # puts @cache_key_name
     end
 
     def obtain_offers
+      # Get deserialized Offer object from cache if found
+      # cached = $redis.get(@cache_key_name)
+      # if cached
+      #   puts "Found key #{@cache_key_name} in cache"
+      #   return Marshal.load(cached)
+      # end
 
+      # If not – run two requests one after another and try to combine them
+      start = Time.now # debugging
       if search_params_make_sense
         json = Avion::QPXRequester.new(
           city: @city,
@@ -21,12 +34,24 @@ module Avion
           starts_on: @starts_on,
           returns_on: @returns_on,
           nb_travelers: @nb_travelers,
-          api_key: ENV['SKYSCANNER_API_KEY']
+          nb_solutions: 3,
+          api_key: ENV['GOOGLE_QPX_API_KEY']
         ).make_request
+
+        # DEBUG ONLY
+        puts "#{@city} - #{@region} request made to QPX"
       else
         raise 'Search params did not make sense'
       end
 
+      finish = Time.now # debugging
+      took_seconds = (finish - start).round(2)
+
+      # Pub-sub part
+      # Notify first request is made
+      # Pusher.trigger('qpx_updates', 'request_made', origin: @city,
+      #                                               destination: @region,
+      #                                               took_seconds: took_seconds)
       @data = JSON.parse(json)
 
       if !@data['trips']['tripOption'].nil?
@@ -35,6 +60,13 @@ module Avion
         rtf = []
       end
       @rtf = rtf
+
+      # $redis.set(@cache_key_name, Marshal.dump(output))
+
+      # # Notify we are ready to return request data
+      # Pusher.trigger('qpx_updates', 'requests_completed', increment: 1,
+      #                                                     roundtrips_analyzed: output.length)
+
 
     end
 
@@ -55,6 +87,10 @@ module Avion
       (Date.parse(@starts_on) < Date.parse(@returns_on))
     end
 
+    # TODO: DRY with offers controller and check_against_cache
+    # def generate_cache_key_name
+    #   alphabetical = [@origin_a, @destination_city].sort
+    #   "#{alphabetical.first}_#{alphabetical.last}_#{@destination_city}_#{@date_there}_#{@date_back}"
+    # end
   end
-
 end
