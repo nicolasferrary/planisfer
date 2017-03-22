b0ab813183604fdc9e80da30da9911bcd0f8a2cd
Nicolas Ferrary <ferraryn@gmail.com>
2017-03-22 18:23:19 +0100

    nex arrow

--- app/controllers/car_rentals_controller.rb
+++ app/controllers/car_rentals_controller.rb
@@ -19,19 +19,12 @@ class CarRentalsController < ApplicationController
     # @car_rentals is an array of instances of car_rentals
     # Uncomment if you want to test with a seed
     # @car_rentals = [CarRental.all[0], CarRental.all[1], CarRental.all[2], CarRental.all[3], CarRental.all[4]]
-    @category_index = {
-      :mini => 0,
-      :economy => 0,
-      :compact => 0,
-      :standard => 0,
-      :fullsize => 0,
-      :premium => 0,
-    }
-    @car_selection = get_best_cars_per_category(@car_rentals, @category_index)
+    @car_selection = get_best_cars_per_category(@car_rentals)
     # @car_selection is a hash of instances of car_rentals (1 instance per car category)
 
     # This is just for test
-     @times = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM" ]
+    @times = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM" ]
+    @next_photos = define_next_photo_params(@car_rentals)
   end
 
   private
@@ -50,7 +43,8 @@ class CarRentalsController < ApplicationController
       return car_rentals
   end
 
-  def get_best_cars_per_category(rentals, category_index)
+  def get_best_cars_per_category(rentals)
+    category_index = define_category_index
     best_cars = {}
     best_cars[:mini] = get_best_car(rentals, "Mini", category_index[:mini])
     best_cars[:economy] = get_best_car(rentals, "Economy", category_index[:economy])
@@ -66,6 +60,65 @@ class CarRentalsController < ApplicationController
     sorted_cars = cars.sort_by { |rental| rental.price }
     best_car = sorted_cars[index]
   end
+
+  def define_category_index
+    category_index = {
+      :mini => params[:mini_index].to_i || 0,
+      :economy => 0,
+      :compact => 0,
+      :standard => 0,
+      :fullsize => 0,
+      :premium => 0,
+    }
+  end
+
+  def define_next_photo_params(car_rentals)
+    mini_max_param = [4, number_rentals(car_rentals, "Mini")-1].min
+    economy_max_param = [4, number_rentals(car_rentals, "Economy")-1].min
+    compact_max_param = [4, number_rentals(car_rentals, "Compact")-1].min
+    standard_max_param = [4, number_rentals(car_rentals, "Intermediate/Standard")-1].min
+    fullsize_max_param = [4, number_rentals(car_rentals, "Fullsize")-1].min
+    premium_max_param = [4, number_rentals(car_rentals, "Premium/Luxury")-1].min
+    next_photo_params = {}
+    if params[:mini_index].to_i == mini_max_param
+      next_photo_params[:mini] = 0
+    else
+      next_photo_params[:mini] = params[:mini_index].to_i + 1
+    end
+    if params[:economy_index].to_i == economy_max_param
+      next_photo_params[:economy] = 0
+    else
+      next_photo_params[:economy] = params[:economy_index].to_i + 1
+    end
+    if params[:compact_index].to_i == compact_max_param
+      next_photo_params[:compact] = 0
+    else
+      next_photo_params[:compact] = params[:compact_index].to_i + 1
+    end
+     if params[:standard_index].to_i == standard_max_param
+      next_photo_params[:standard] = 0
+    else
+      next_photo_params[:standard] = params[:standard_index].to_i + 1
+    end
+     if params[:fullsize_index].to_i == fullsize_max_param
+      next_photo_params[:fullsize] = 0
+    else
+      next_photo_params[:fullsize] = params[:fullsize_index].to_i + 1
+    end
+     if params[:premium_index].to_i == premium_max_param
+      next_photo_params[:premium] = 0
+    else
+      next_photo_params[:premium] = params[:premium_index].to_i + 1
+    end
+
+    next_photo_params
+  end
+
+  def number_rentals(rentals, category)
+    category_rentals = rentals.select {|rental| rental.car.category == category}
+    category_rentals.count
+  end
+
 
 end
 
