a8973d83f25bf68c4c2f1695f6c6b5295b79e4ef
Nicolas Ferrary <ferraryn@gmail.com>
2017-04-03 13:53:31 +0200

    factorized currency

--- app/services/avion/qpx_requester.rb
+++ app/services/avion/qpx_requester.rb
@@ -13,15 +13,16 @@ module Avion
       @nb_solutions = args[:nb_solutions]
       @nb_travelers = args[:nb_travelers]
       @api_key = args[:api_key]
+      @currency = "EUR"
     end
 
     # TODO: Account for 400
     # RestClient::BadRequest: 400 Bad Request
     def make_request
       if !@return.nil?
-        url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=' + @api_key + '&origin=' + @origin +'&destination=' + @destination + '&departure_date=' + @departure + '&return_date=' + @return + '&adults=' + @nb_travelers + '&nonstop=true' + '&currency=EUR' + '&number_of_results=' + @nb_solutions.to_s
+        url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=' + @api_key + '&origin=' + @origin +'&destination=' + @destination + '&departure_date=' + @departure + '&return_date=' + @return + '&adults=' + @nb_travelers + '&nonstop=true' + '&currency=' + @currency + '&number_of_results=' + @nb_solutions.to_s
       else
-        url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=' + @api_key + '&origin=' + @origin +'&destination=' + @destination + '&departure_date=' + @departure + '&adults=' + @nb_travelers + '&nonstop=true' + '&currency=EUR' + '&number_of_results=' + @nb_solutions.to_s
+        url = 'https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=' + @api_key + '&origin=' + @origin +'&destination=' + @destination + '&departure_date=' + @departure + '&adults=' + @nb_travelers + '&nonstop=true' + '&currency=' + @currency + '&number_of_results=' + @nb_solutions.to_s
       end
       response = RestClient.get(url, content_type: :json, accept: :json)
       response.body
