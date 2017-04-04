679ace48b0eb2b3a71f3c29e44dcfef16f18ab9a
Nicolas Ferrary <ferraryn@gmail.com>
2017-04-04 13:16:30 +0200

    call works

--- app/services/rental/rental_requester.rb
+++ app/services/rental/rental_requester.rb
@@ -15,25 +15,78 @@ module Rental
       @locale = "en-GB"
       @api_key = args[:api_key]
     end
-
     def make_request
     # Authentification see here (https://developer.sabre.com/docs/rest_basics/authentication)
       # Client ID
         # V1:7z0cif7m6wb63vrs:DEVCENTER:EXT
       # Base64 Encoded credentials - intermediary step
-        # VjE6N3owY2lmN202d2I2M3ZyczpERVZDRU5URVI6RVhUDQo=
+        # VjE6N3owY2lmN202d2I2M3ZyczpERVZDRU5URVI6RVhU
       # Encoded password : ZjhpdEhKTTM=
       # Base64 Encoded credentials - final step
-      encoded_credentials = 'VmpFNk4zb3dZMmxtTjIwMmQySTJNM1p5Y3pwRVJWWkRSVTVVUlZJNlJWaFVEUW89OmY4aXRISk0z'
+      encoded_credentials = 'VmpFNk4zb3dZMmxtTjIwMmQySTJNM1p5Y3pwRVJWWkRSVTVVUlZJNlJWaFU6WmpocGRFaEtUVE09'
 
       # Get an access token
-        url = 'https://api.test.sabre.com/v2/auth/token'
-        response = RestClient.post url, 'grant_type=client_credentials', {authorization: 'Basic ' + encoded_credentials, :content_type => 'application/x-www-form-urlencoded' }
-# Invalid request 400 "Invalid payload: grant_type must be specified"
+      url = 'https://api.test.sabre.com/v2/auth/token'
+      response = RestClient.post url, 'grant_type=client_credentials', {authorization: 'Basic ' + encoded_credentials, :content_type => 'application/x-www-form-urlencoded', accept: :json }
+      access_token = eval(response.body)[:access_token]
+
+      # TODO : To avoid calling new access_token every time, let's stock tokens in a tab and call them unless they are expired (> 600 000 sec) ==> Use https://developer.sabre.com/resources/best_practices#handling-expirations
+
+    # Call the Car Availability Rest API
+      car_url = "https://api.test.sabre.com/v2.4.0/shop/cars"
+      request_body = compose_body_request
+      response = RestClient.post car_url, request_body, {authorization: 'Bearer ' + access_token, :content_type => 'application/json'}
     end
 
     private
 
+    def compose_body_request
+
+      request_hash = {
+    "OTA_VehAvailRateRQ": {
+      "VehAvailRQCore": {
+        "QueryType": "Shop",
+        "RateQualifier": {
+          "CurrencyCode": @currency
+          },
+        "VehRentalCore": {
+          "PickUpDateTime": @pick_up_date_time.strftime("%m-%dT%H:%M"),
+          "ReturnDateTime": @drop_off_date_time.strftime("%m-%dT%H:%M"),
+          "PickUpLocation": {
+            "LocationCode": @pick_up_place
+          },
+          "ReturnLocation": {
+            "LocationCode": @drop_off_place
+          }
+        }
+      }
+    }
+}
+
+#"07-07T09:00"
+
+      # request_hash = {
+      #   "OTA_VehAvailRateRQ" => {
+      #   "VehAvailRQCore" => {
+      #     "QueryType" => "Shop",
+      #     "RateQualifier" => {
+      #           "CurrencyCode" => @currency
+      #           },
+      #     "VehRentalCore" => {
+      #       "PickUpDateTime" => @pick_up_date_time.to_s,
+      #       "ReturnDateTime" => @drop_off_date_time.to_s,
+      #       "PickUpLocation" => {
+      #         "LocationCode" => @pick_up_place.to_s
+      #       },
+      #       "ReturnLocation" => {
+      #         "LocationCode" => @drop_off_place.to_s
+      #         }
+      #       }
+      #     }
+      #   }
+      # }
+      json = JSON.generate(request_hash)
+    end
 
 # OLD - Skyscanner api request
     # def make_request
