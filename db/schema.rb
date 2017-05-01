c0a3b035c893db9b78134cfbb8a691226e3d9670
Nicolas Ferrary <ferraryn@gmail.com>
2017-05-01 13:51:48 +0200

    added description and pois to regions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501110709) do
+ActiveRecord::Schema.define(version: 20170501114041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,8 +77,10 @@ ActiveRecord::Schema.define(version: 20170501110709) do
 
   create_table "regions", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "description"
+    t.text     "pois",        default: [],              array: true
   end
 
   create_table "round_trip_flights", force: :cascade do |t|
