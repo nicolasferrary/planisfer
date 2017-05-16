6a7ddae63fdcdeacfffd5cb83655e449e650eda7
Nicolas Ferrary <ferraryn@gmail.com>
2017-05-16 17:42:02 +0200

    refresh map trips route works

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :searches, only: [:create, :show]
-  resources :trips, only: [:index, :show, :update, :create]
-  resources :round_trip_flights, only: [:create] do
+  resources :trips, only: [:index, :show, :update, :create]do
     get :refresh_map, to: "searches#refresh_map"
   end
+  resources :round_trip_flights, only: [:create]
   resources :selections, only: [:create, :show]
   resources :airports
   resources :orders, only: [:show, :create] do
