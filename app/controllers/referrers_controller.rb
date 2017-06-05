5c17fbdc5f83dea76e3770b3a404af5f67e24b19
Nicolas Ferrary <ferraryn@gmail.com>
2017-06-05 11:23:43 +0200

    routing works

---
+++ app/controllers/referrers_controller.rb
@@ -0,0 +1,19 @@
+class ReferrersController < ApplicationController
+  skip_before_action :authenticate_member!, only: :check
+
+  def check
+    @referral_email = params[:email]
+    @referrers_emails = []
+    Member.all.each do |member|
+      @referrers_emails << member.email
+    end
+
+    if @referrers_emails.include?(@referral_email)
+      return redirect_to new_member_registration_path(referrer: "checked")
+    else
+      flash[:search_error] = "This person does not seem to be a Planisfer member."
+      return redirect_to new_member_session_path(referrer: 'wrong_credentials')
+    end
+
+  end
+end
