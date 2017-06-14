4b52204a9c44d42e8f8690a483a77c545c0ab38e
Nicolas Ferrary <ferraryn@gmail.com>
2017-06-14 15:39:53 +0200

    saving

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true,
-    callback_url: "http://www.planisfer.space//members/auth/facebook/callback"
+    callback_url: "http://www.planisfer.space/members/auth/facebook/callback"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
