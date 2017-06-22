8fcfe9f8f3638db7f9537953fc1ad443f16d4ca4
Nicolas Ferrary <ferraryn@gmail.com>
2017-06-22 15:41:55 +0200

    getting params

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,6 +1,8 @@
 class ExperiencesController < ApplicationController
 
   def create_experiences
-
+    # For each region in params, create an experience
+    @regions = params
+    # Renvoyer vers la new de la première expérience
   end
 end
