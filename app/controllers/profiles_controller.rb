e3fd14c252fdd00430c3780e57ed586732ca3988
Nicolas Ferrary <ferraryn@gmail.com>
2017-06-27 17:36:35 +0200

    selected regions visible on profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,15 @@ class ProfilesController < ApplicationController
   def show
     @member = current_member
     @regions = Region.all
-    # @no_feedback =
+    @selected_regions = define_selected_regions(@member)
+  end
+
+  def define_selected_regions(member)
+    selected_regions = []
+    member.experiences.each do |exp|
+      selected_regions << exp.region
+    end
+    selected_regions
   end
 
 end
