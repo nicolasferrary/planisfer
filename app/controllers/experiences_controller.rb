fcd874d00540ddeb6ec4c24cf053c64a4683e94d
Nicolas Ferrary <ferraryn@gmail.com>
2017-06-27 15:14:09 +0200

    solved checkbox pb

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -18,19 +18,22 @@ class ExperiencesController < ApplicationController
     end
 
     new_regions.each_with_index do |region, index|
-      if index >=1
-        Experience.create(member: current_member, region: region)
-      end
+      Experience.create(member: current_member, region: region)
     end
-    first_region = new_regions.first
-    @first_experience = Experience.create(member: current_member, region: first_region)
+
     redirect_to experiences_path
   end
 
   def index
+
     @experiences = current_member.experiences.to_a
-    @experience = Experience.first
+    @reviewed_experiences = create_reviewed_exp_array(@experiences)
+    @non_reviewed_experiences = @experiences - @reviewed_experiences
+    @clicked_exp_id = params[:clicked] || nil
+    @experience = define_focused_experience(@non_reviewed_experiences, @clicked_exp_id, @experiences.first)
+    put_first_exp_first_in_array(@experience, @experiences)
     @categories = ["Honeymoon", "Road trip", "Family friendly", "Nature/ Sport", "Cultural", "Relaxing", "Big fiesta", "Local immersion"]
+
   end
 
   def update
@@ -40,6 +43,32 @@ class ExperiencesController < ApplicationController
     @experience.category = params[:categories]
     @experience.save
     redirect_to new_experience_subexperience_path(experience_id: @experience.id)
+  end
+
+  def create_reviewed_exp_array(experiences)
+      reviewed_exp = []
+      experiences.each do |exp|
+        reviewed_exp << exp if exp.subexperiences != []
+      end
+      reviewed_exp
+  end
+
+  def define_focused_experience(non_reviewed_experiences, clicked_exp_id, first_experience)
+    # the one clicked
+    if !clicked_exp_id.nil?
+      focused_exp = Experience.find(clicked_exp_id)
+    # The first that is not reviewed
+    elsif non_reviewed_experiences != []
+      focused_exp = non_reviewed_experiences.first
+    # The first one
+    else
+      focused_exp = first_experience
+    end
+  end
+
+  def put_first_exp_first_in_array(experience, experiences)
+    experiences.delete(experience)
+    experiences = experiences.insert(0,experience)
   end
 
 end
