14e1770d692b428c409ef4c9a5dbc119d2a40dc1
Nicolas Ferrary <ferraryn@gmail.com>
2017-06-29 13:27:12 +0200

    new content for home works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   def home
     update_member_status
     @status = current_member.profile_status
+    @destination_name = params[:destination] || nil
+    @destination = Region.find_by_name(@destination_name) if !@destination_name.nil?
+    @main_title = build_main_title(@destination_name)
     @step1_title = build_title(@status)
     @step1_subtitle = build_subtitle(@status)
     respond_to do |format|
@@ -27,6 +30,15 @@ class PagesController < ApplicationController
   def build_subtitle(status)
     "(#{status}% completed)" unless status == 0 || status == 100
   end
+
+  def build_main_title(destination)
+    if destination.nil?
+      main_title = "Prepare and book fully customized trips"
+    else
+      main_title = "Prepare and book a fully customized trip to #{destination}"
+    end
+  end
+
 
 end
 
